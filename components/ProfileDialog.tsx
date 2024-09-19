ca1c0bd02e2aa563547a6ae3daedc3fd581ff436
Phuoc Le <lvphuocqt@gmail.com>
2024-09-19 10:40:58 +0700

    fix ui button

--- components/ProfileDialog.tsx
+++ components/ProfileDialog.tsx
@@ -390,6 +390,17 @@ export function ProfileDialog({
                             : "Đang duyệt chỉnh sửa"}
                         </Button>
                       )}
+                      {(data?.hasAddReq ||
+                        data?.hasDeleteReq ||
+                        data?.hasEditReq) && (
+                        <div className="flex gap-1 h-[30px]">
+                          <span>người Yêu Cầu: </span>
+                          <span className="font-bold">
+                            {" "}
+                            {`${node?.editUser}`}
+                          </span>
+                        </div>
+                      )}
                     </>
                   )}
                 </>
@@ -418,20 +429,34 @@ export function ProfileDialog({
             {mode === "view" && (
               <div className="flex gap-4 justify-center items-center">
                 <AuthButton
-                  variant="default"
-                  className="w-[100px] mb-2 "
+                  className="mb-2 w-[120px] bg-blue-500 hover:bg-blue-600 text-white rounded transition duration-300 ease-in-out"
                   onClick={() => {
                     setMode("edit");
                     setData({ ...(node as any) });
                   }}
                   disabled={node?.hasEditReq || node?.hasDeleteReq}
                 >
+                  <svg
+                    stroke="currentColor"
+                    fill="currentColor"
+                    stroke-width="0"
+                    viewBox="0 0 576 512"
+                    data-pc-el-id="FaEdit-59-59"
+                    height="16px"
+                    width="16px"
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="mr-1"
+                  >
+                    <path d="M402.6 83.2l90.2 90.2c3.8 3.8 3.8 10 0 13.8L274.4 405.6l-92.8 10.3c-12.4 1.4-22.9-9.1-21.5-21.5l10.3-92.8L388.8 83.2c3.8-3.8 10-3.8 13.8 0zm162-22.9l-48.8-48.8c-15.2-15.2-39.9-15.2-55.2 0l-35.4 35.4c-3.8 3.8-3.8 10 0 13.8l90.2 90.2c3.8 3.8 10 3.8 13.8 0l35.4-35.4c15.2-15.3 15.2-40 0-55.2zM384 346.2V448H64V128h229.8c3.2 0 6.2-1.3 8.5-3.5l40-40c7.6-7.6 2.2-20.5-8.5-20.5H48C21.5 64 0 85.5 0 112v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V306.2c0-10.7-12.9-16-20.5-8.5l-40 40c-2.2 2.3-3.5 5.3-3.5 8.5z"></path>
+                  </svg>
                   Chỉnh sửa
                 </AuthButton>
+
                 {loggedInUser ? (
                   !isLeader && !isRootNode ? (
                     <>
                       <Button
+                        className="mb-2 w-[80px] bg-red-500 hover:bg-red-600 text-white  rounded transition duration-300 ease-in-out"
                         onClick={() => {
                           setOpenAlert({
                             messenger:
@@ -445,10 +470,21 @@ export function ProfileDialog({
                             },
                           });
                         }}
-                        variant="default"
-                        className="w-[70px] mb-2 "
                         disabled={node?.hasEditReq || node?.hasDeleteReq}
                       >
+                        <svg
+                          stroke="currentColor"
+                          fill="currentColor"
+                          stroke-width="0"
+                          viewBox="0 0 448 512"
+                          data-pc-el-id="FaTrash-66-66"
+                          height="15px"
+                          width="15px"
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="mr-1"
+                        >
+                          <path d="M432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16zM53.2 467a48 48 0 0 0 47.9 45h245.8a48 48 0 0 0 47.9-45L416 128H32z"></path>
+                        </svg>
                         Xoá
                       </Button>
                     </>
@@ -457,7 +493,7 @@ export function ProfileDialog({
                       <DropdownMenu>
                         <DropdownMenuTrigger asChild>
                           <Button
-                            className="w-[120px] mb-2 "
+                            className="w-[140px] mb-2 "
                             variant="default"
                             disabled={node?.hasEditReq || node?.hasDeleteReq}
                           >
@@ -488,6 +524,7 @@ export function ProfileDialog({
                         </DropdownMenuContent>
                       </DropdownMenu>
                       <Button
+                        className="w-[70px] mb-2 bg-red-500 hover:bg-red-600 text-white  rounded transition duration-300 ease-in-out"
                         onClick={() => {
                           setOpenAlert({
                             messenger:
@@ -501,10 +538,21 @@ export function ProfileDialog({
                             },
                           });
                         }}
-                        variant="default"
-                        className="w-[70px] mb-2 "
                         disabled={node?.hasEditReq || node?.hasDeleteReq}
                       >
+                        <svg
+                          stroke="currentColor"
+                          fill="currentColor"
+                          stroke-width="0"
+                          viewBox="0 0 448 512"
+                          data-pc-el-id="FaTrash-66-66"
+                          height="15px"
+                          width="15px"
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="mr-1"
+                        >
+                          <path d="M432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16zM53.2 467a48 48 0 0 0 47.9 45h245.8a48 48 0 0 0 47.9-45L416 128H32z"></path>
+                        </svg>
                         Xoá
                       </Button>
                     </>
@@ -514,7 +562,20 @@ export function ProfileDialog({
                     <AuthButton variant="default" className="w-[150px] mb-2  ">
                       Thêm Thành viên
                     </AuthButton>
-                    <AuthButton variant="default" className="w-[150px] mb-2  ">
+                    <AuthButton className="w-[80px] mb-2 bg-red-500 hover:bg-red-600 text-white  rounded transition duration-300 ease-in-out">
+                      <svg
+                        stroke="currentColor"
+                        fill="currentColor"
+                        stroke-width="0"
+                        viewBox="0 0 448 512"
+                        data-pc-el-id="FaTrash-66-66"
+                        height="15px"
+                        width="15px"
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="mr-1"
+                      >
+                        <path d="M432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16zM53.2 467a48 48 0 0 0 47.9 45h245.8a48 48 0 0 0 47.9-45L416 128H32z"></path>
+                      </svg>
                       Xoá
                     </AuthButton>
                   </>
