c00c514820d1406436fee30d85a0de8ef9cdab5f
Phuoc Le <lvphuocqt@gmail.com>
2024-09-20 14:30:35 +0700

    fix UI

--- components/ProfileDialog.tsx
+++ components/ProfileDialog.tsx
@@ -450,7 +450,7 @@ export function ProfileDialog({
             {mode === "view" && (
               <div className="flex gap-4 justify-center items-center">
                 <AuthButton
-                  className="mb-2 w-[120px] bg-blue-500 hover:bg-blue-600 text-white rounded transition duration-300 ease-in-out"
+                  className="mb-2 w-[120px] px-1 bg-blue-500 hover:bg-blue-600 text-white rounded transition duration-300 ease-in-out"
                   onClick={() => {
                     setMode("edit");
                     setData({ ...(node as any) });
@@ -466,7 +466,7 @@ export function ProfileDialog({
                     height="16px"
                     width="16px"
                     xmlns="http://www.w3.org/2000/svg"
-                    className="mr-1"
+                    className="mr-2"
                   >
                     <path d="M402.6 83.2l90.2 90.2c3.8 3.8 3.8 10 0 13.8L274.4 405.6l-92.8 10.3c-12.4 1.4-22.9-9.1-21.5-21.5l10.3-92.8L388.8 83.2c3.8-3.8 10-3.8 13.8 0zm162-22.9l-48.8-48.8c-15.2-15.2-39.9-15.2-55.2 0l-35.4 35.4c-3.8 3.8-3.8 10 0 13.8l90.2 90.2c3.8 3.8 10 3.8 13.8 0l35.4-35.4c15.2-15.3 15.2-40 0-55.2zM384 346.2V448H64V128h229.8c3.2 0 6.2-1.3 8.5-3.5l40-40c7.6-7.6 2.2-20.5-8.5-20.5H48C21.5 64 0 85.5 0 112v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V306.2c0-10.7-12.9-16-20.5-8.5l-40 40c-2.2 2.3-3.5 5.3-3.5 8.5z"></path>
                   </svg>
@@ -477,7 +477,7 @@ export function ProfileDialog({
                   !isLeader && !isRootNode ? (
                     <>
                       <Button
-                        className="mb-2 w-[80px] bg-red-500 hover:bg-red-600 text-white  rounded transition duration-300 ease-in-out"
+                        className="mb-2 px-1 w-[80px] bg-red-500 hover:bg-red-600 text-white  rounded transition duration-300 ease-in-out"
                         onClick={() => {
                           setOpenAlert({
                             messenger:
@@ -502,7 +502,7 @@ export function ProfileDialog({
                           height="15px"
                           width="15px"
                           xmlns="http://www.w3.org/2000/svg"
-                          className="mr-1"
+                          className="mr-2"
                         >
                           <path d="M432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16zM53.2 467a48 48 0 0 0 47.9 45h245.8a48 48 0 0 0 47.9-45L416 128H32z"></path>
                         </svg>
@@ -524,7 +524,7 @@ export function ProfileDialog({
                               height="20px"
                               viewBox="0 0 24 24"
                               fill="none"
-                              className="mr-1"
+                              className="mr-2"
                             >
                               <path
                                 fill-rule="evenodd"
@@ -560,7 +560,7 @@ export function ProfileDialog({
                         </DropdownMenuContent>
                       </DropdownMenu>
                       <Button
-                        className="w-[80px] mb-2 bg-red-500 hover:bg-red-600 text-white  rounded transition duration-300 ease-in-out"
+                        className="w-[80px] px-1 mb-2 bg-red-500 hover:bg-red-600 text-white  rounded transition duration-300 ease-in-out"
                         onClick={() => {
                           setOpenAlert({
                             messenger:
@@ -585,7 +585,7 @@ export function ProfileDialog({
                           height="15px"
                           width="15px"
                           xmlns="http://www.w3.org/2000/svg"
-                          className="mr-1"
+                          className="mr-2"
                         >
                           <path d="M432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16zM53.2 467a48 48 0 0 0 47.9 45h245.8a48 48 0 0 0 47.9-45L416 128H32z"></path>
                         </svg>
@@ -606,7 +606,7 @@ export function ProfileDialog({
                         height="20px"
                         viewBox="0 0 24 24"
                         fill="none"
-                        className="mr-1"
+                        className="mr-2"
                       >
                         <path
                           fill-rule="evenodd"
@@ -630,7 +630,7 @@ export function ProfileDialog({
                         height="15px"
                         width="15px"
                         xmlns="http://www.w3.org/2000/svg"
-                        className="mr-1"
+                        className="mr-2"
                       >
                         <path d="M432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16zM53.2 467a48 48 0 0 0 47.9 45h245.8a48 48 0 0 0 47.9-45L416 128H32z"></path>
                       </svg>
@@ -658,7 +658,7 @@ export function ProfileDialog({
                   variant="destructive"
                   className="w-[70px] mb-2 "
                 >
-                  Restore
+                  Khôi phục
                 </Button>
               </div>
             )}
